6807c78a242b38df7f850d0b0706de82f71bbc6a
Despire1 <123311449+Despire1@users.noreply.github.com>
2026-01-21 17:16:31 +0300

    Add student notification templates

---
+++ src/shared/lib/notificationTemplates.ts
@@ -0,0 +1,23 @@
+export const DEFAULT_STUDENT_UPCOMING_LESSON_TEMPLATE =
+  'Здравствуйте, {{student_name}}! Напоминаю: занятие {{lesson_date}} в {{lesson_time}}. До встречи 🙂';
+
+export const DEFAULT_STUDENT_PAYMENT_DUE_TEMPLATE =
+  'Здравствуйте, {{student_name}}! Напоминание об оплате занятия {{lesson_date}} ({{lesson_time}}). Сумма: {{lesson_price}} ₽. Спасибо 🙂';
+
+export const STUDENT_LESSON_TEMPLATE_VARIABLES = [
+  'student_name',
+  'lesson_date',
+  'lesson_time',
+  'lesson_datetime',
+] as const;
+
+export const STUDENT_PAYMENT_TEMPLATE_VARIABLES = [
+  'student_name',
+  'lesson_date',
+  'lesson_time',
+  'lesson_datetime',
+  'lesson_price',
+] as const;
+
+export type StudentLessonTemplateVariable = (typeof STUDENT_LESSON_TEMPLATE_VARIABLES)[number];
+export type StudentPaymentTemplateVariable = (typeof STUDENT_PAYMENT_TEMPLATE_VARIABLES)[number];
