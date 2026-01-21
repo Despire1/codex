97a0b7547064496be642efa16c41bbd4c8c3de9a
Despire1 <123311449+Despire1@users.noreply.github.com>
2026-01-22 01:17:11 +0300

    Make meeting link icon smaller

--- src/widgets/schedule/ScheduleSection.tsx
+++ src/widgets/schedule/ScheduleSection.tsx
@@ -459,7 +459,7 @@ export const ScheduleSection: FC<ScheduleSectionProps> = ({
         aria-label="Открыть ссылку на занятие"
         data-testid={`lesson-item-open-link-${lesson.id}`}
       >
-        <LinkIcon width={14} height={14} />
+        <LinkIcon width={10} height={10} />
       </button>
     );
   };
